a9d2ab929bee7a37b8a6cfc55df8468967f1a08b
TheCreativeSpark <thecreativespark@proton.me>
2024-01-05 18:50:12 +0530

    bubble sort

--- dsa.h
+++ dsa.h
@@ -12,7 +12,17 @@ int linearSearch(int array[],int size, int key){
 
 
 
-
+void bubleSort(int arr[], int size){
+    for(int i=0; i<size-1; i++){
+        for(int j=0; j<size-1-i; j++){
+            if(arr[j]>arr[j+1]){
+                int temp=arr[j];
+                arr[j] = arr[j+1];
+                arr[j+1] = temp;
+            }
+        }
+    }
+}
 
 
 
